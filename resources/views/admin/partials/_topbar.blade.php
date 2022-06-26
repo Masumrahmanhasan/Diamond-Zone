811838e8a01f5eece16feb45ec12729de87f60cb
MASUM RAHMAN <masumrahmanhasan@gmail.com>
2022-06-26 15:29:33 -0700

    added some data for admin panel

--- resources/views/admin/partials/_topbar.blade.php
+++ resources/views/admin/partials/_topbar.blade.php
@@ -23,7 +23,7 @@
 						<ul class="navbar-nav align-items-center">
 								<li class="nav-item dropdown">
 										<a class="nav-link text-dark"
-												href="{{ route('home') }}" role="button" target="_blank">
+												href="{{ url('/') }}" role="button" target="_blank">
                                                 <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-sm text-gray-900" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                                     <path stroke-linecap="round" stroke-linejoin="round" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                                                 </svg>
